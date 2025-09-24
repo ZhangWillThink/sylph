6a5a2d1753808d7ece94b95cb411de7e3b7b6372
ZhangWill <zwillthink@outlook.com>
2025-09-24 16:45:46 +0800

    Refactor code structure for improved readability and maintainability

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,6 +1,18 @@
+import Header from '~/components/Header'
+
 export default defineComponent({
   name: 'IndexPage',
   setup() {
-    return () => <div class="text-night-sky">Welcome to Nuxt 3!</div>
+    return () => (
+      <main class="h-svh w-svw overflow-hidden bg-cover bg-top bg-no-repeat">
+        <Header />
+
+        <section class="flex justify-between sm:gap-4 md:gap-7 lg:gap-10">
+          <aside>123</aside>
+
+          <aside>12313</aside>
+        </section>
+      </main>
+    )
   },
 })
